ce4de13eb3c35ac727b1693f40b6a0103e201959
dlsso <supertonesfan@hotmail.com>
2014-06-24 16:17:37 -0600

    Solved 2 and logged instructions to console

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 // Problem 1
 
 var letterCapitalize = function(string) {
-	var words = string.split(" ")
-	console.log(words)
-	
+	var words = string.split(" ");
 
 	for(var i=0; i < words.length; i++) {
 		words[i] = words[i][0].toUpperCase() + words[i].slice(1);
@@ -12,5 +10,20 @@ var letterCapitalize = function(string) {
 	return words.join(" ");
 }
 
+console.log("1. Use letterCapitalize('some string') to test.")
+
+
+
 
 // Problem 2
+var wordCount = function(string) {
+	var words = string.split(" ");
+	return words.length
+}
+
+console.log("2. Use wordCount('some string') to test.")
+
+
+
+
+// Problem 3
